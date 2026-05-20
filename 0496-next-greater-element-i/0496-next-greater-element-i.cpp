8a5a949f41ecf43789b1cd1d5a4e216188431f8e
saimaheshgudla <saimaheshgudla801@gmail.com>
2026-05-20 12:31:38 +0530

    Time: 0 ms (100%), Space: 12.9 MB (35.3%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int>st;
+        map<int,int>mpp;
+           for(int i=nums2.size()-1;i>=0;i--){
+                while(!st.empty() && st.top()<=nums2[i]){
+                    st.pop();
+                }
+
+                if(st.empty()){
+                    mpp[nums2[i]] = -1;
+                }
+                else{
+                    mpp[nums2[i]] = st.top();
+                }
+
+                st.push(nums2[i]);
+           }
+        for(int i=0;i<nums1.size();i++){
+            auto it = mpp.find(nums1[i]);
+            if(it != mpp.end()){
+                nums1[i]=it->second;
+            }
+        }
+        return nums1;
+    }
+};
