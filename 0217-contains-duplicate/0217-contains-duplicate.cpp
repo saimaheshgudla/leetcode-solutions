6f6106ad69653ce06068128bd991871d53ea4aed
saimaheshgudla <saimaheshgudla801@gmail.com>
2026-08-15 12:41:00 +0530

    Time: 79 ms (24.78%), Space: 111.5 MB (22.69%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int,int> mpp;
+        unordered_map<int,int> mpp;
         for(auto it:nums){
             mpp[it]++;
             if(mpp[it]>1) return true;
